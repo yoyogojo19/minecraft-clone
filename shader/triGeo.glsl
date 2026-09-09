6430b58e27e0bf71a98ec1f55ad0028bdea4135b
Yannick Godbout <yannickgodbout32@gmail.com>
2026-09-09 17:40:16 -0400

    clangd setup and cleanup

--- shader/triGeo.glsl
+++ shader/triGeo.glsl
@@ -4,9 +4,24 @@ layout(point) in;
 layout(triangle_strip, max_verticies = 4) out;
 
 in ivec3 extra;
-in vec3 vPos[];
-out vec2 TextCords;
+out vec2 texCoords;
 
 void main() {
-    gl_Position = vec4(vPos[0] + float(extra.x));
+    gl_Position = gl_in[0].gl_Position;
+    texCoords = vec2(0.0, 0.0);
+    EmitVertex();
+
+    gl_Position = gl_in[0].gl_Position + vec4(float(extra.x), 0.0, 0.0, 0.0);
+    texCoords = vec2(1.0, 0.0);
+    EmitVertex();
+
+    gl_Position = gl_in[0].gl_Position + vec4(0.0, float(extra.y), 0.0, 0.0);
+    texCoords = vec2(0.0, 1.0);
+    EmitVertex();
+
+    gl_Position = gl_in[0].gl_Position + vec4(float(extra.x), float(extra.y), 0.0, 0.0);
+    texCoords = vec2(1.0, 1.0);
+    EmitVertex();
+
+    EndPrimitive();
 }
